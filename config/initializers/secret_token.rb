5759662291b2ef3258abe715fd2243aac82eb9a6
Michał Krajewski <mkrajewski90@gmail.com>
2012-02-05 14:28:15 +0100

    Initialize

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Voyager::Application.config.secret_token = '6953f2d178aeb239a1683467cbdf40a0bf796c34664f1ceee7a7829cfe7a3dc108cba0ef652e6da395fdf26133a609428f8079dc8e02c0488fb1e5681563d8a7'
