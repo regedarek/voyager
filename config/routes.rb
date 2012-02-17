a68a1fc95cb24f70830ab96ef6ddf191b8b5fc9f
Michał Krajewski <mkrajewski90@gmail.com>
2012-02-17 23:59:23 +0100

    add routing to root path)

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Voyager::Application.routes.draw do
-  get "home/index"
+  root :to => "home#index"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -47,10 +47,6 @@ Voyager::Application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
 
   # See how all your routes lay out with "rake routes"
 
