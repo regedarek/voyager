8472da8d3aa9f7aa85ec0c25052e45d21cd9d5c4
Michał Krajewski <mkrajewski90@gmail.com>
2012-02-18 00:03:04 +0100

    Homepage step pass

--- features/step_definitions/homepage_step.rb
+++ features/step_definitions/homepage_step.rb
@@ -3,10 +3,10 @@ end
 
 When /^I go to the homepage$/ do
   visit(root_path)
-  #pending # express the regexp above with the code you wish you had
 end
 
 Then /^I should be on the homepage$/ do
-  pending # express the regexp above with the code you wish you had
+  current_path == root_path
+  # pending # express the regexp above with the code you wish you had
 end
 
