1fbdebcab7ee84f30a80f9992c220f08b027c4cb
Michał Krajewski <mkrajewski90@gmail.com>
2012-02-19 11:09:08 +0100

    Code for create User

--- spec/views/users/new.html.haml_spec.rb
+++ spec/views/users/new.html.haml_spec.rb
@@ -1,5 +1,10 @@
 require 'spec_helper'
 
 describe "users/new.html.haml" do
-  pending "add some examples to (or delete) #{__FILE__}"
+  #pending "add some examples to (or delete) #{__FILE__}"
+  it "have a form" do
+    assign(:user, mock_model("User").as_null_object)
+    render
+    rendered.should have_selector("form", :method => "post", "action" => users_path)
+  end
 end
