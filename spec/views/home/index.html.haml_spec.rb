41eddb4d2d3de3baec3bf133da5552f06453bdbc
Michał Krajewski <mkrajewski90@gmail.com>
2012-02-21 13:04:58 +0100

    create layout spec

--- spec/views/home/index.html.haml_spec.rb
+++ spec/views/home/index.html.haml_spec.rb
@@ -4,12 +4,17 @@ describe "home/index.html.haml" do
   describe "navigation" do
     context "authentication" do
       it "renders links for guest" do
+	pending
         render
         rendered.should =~ /Sign up/   
         rendered.should =~ /Log in/
       end
 
-      it "renders links for user"
+      it "renders links for user" do
+	pending
+        render
+        rendered.should =~ /Log out/
+      end
     end
   end
 end
