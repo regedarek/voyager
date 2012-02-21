6ce2acd396cc77ac05591dc04c1cb461d62c5598
Michał Krajewski <mkrajewski90@gmail.com>
2012-02-21 22:30:34 +0100

    navigation spec from home to application views

--- spec/views/home/index.html.haml_spec.rb
+++ spec/views/home/index.html.haml_spec.rb
@@ -1,20 +1,5 @@
 require 'spec_helper'
 
 describe "home/index.html.haml" do
-  describe "navigation" do
-    context "authentication" do
-      it "renders links for guest" do
-	pending
-        render
-        rendered.should =~ /Sign up/   
-        rendered.should =~ /Log in/
-      end
 
-      it "renders links for user" do
-	pending
-        render
-        rendered.should =~ /Log out/
-      end
-    end
-  end
 end
