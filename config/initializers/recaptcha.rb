eff698d9d57e25f9913bdd025a73e6a58c981a64
Michał Krajewski <mkrajewski90@gmail.com>
2012-02-23 11:22:36 +0100

    add public key for heroku

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,5 +1,15 @@
 Recaptcha.configure do |config|
-  config.public_key  = '6Ld_Ac4SAAAAAFVxoJgwcJN3VSAbZxYNCPOyxwaP'
-  config.private_key = '6Ld_Ac4SAAAAAJYwHAloMYvAtpxFadXEjd9bymik'
+
+  # => config heroku for voyager-dev.herokuapp.com
+  config.public_key  = '6LebC84SAAAAABAn97z9oWHBXMCzXNbNmXAnsM9V'
+  config.private_key = '6LebC84SAAAAAA_lObvKlkI0g_qRrvGkzG3vEs_A '
   # config.proxy = 'http://127.0.0.0:3000'
+
+
+  # => config heroku for voyager.herokuapp.com
+  # config.public_key  = '6LecC84SAAAAAICHveafvdhmGpv0DBz9cmfEM70y '
+  # config.private_key = '6LecC84SAAAAAMmpLHIbcOC7hKVlItjBQzPNIrRW '
+  # config.proxy = 'http://127.0.0.0:3000'
+
+
 end
