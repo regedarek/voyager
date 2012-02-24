d1a866ed64fb974e8fa9c8c45d053c9d3f781de7
Michał Krajewski <mkrajewski90@gmail.com>
2012-02-24 12:09:02 +0100

    spec for logout routing

--- spec/routing/sessions_routing_spec.rb
+++ spec/routing/sessions_routing_spec.rb
@@ -8,4 +8,8 @@ describe "routes to Sessions" do
   it "routes sessions_path to sessions#create" do
     post(sessions_path).should route_to("sessions#create")
   end
+
+  it "routes logout_path to sessions#destroy" do
+    get(logout_path).should route_to("sessions#destroy")
+  end
 end
