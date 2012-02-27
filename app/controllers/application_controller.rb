eb50e2d51722b4572e00c86c1348d03e7d01e28e
Michał Krajewski <mkrajewski90@gmail.com>
2012-02-27 09:30:32 +0100

    require login for some pages pass

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,19 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-
+  
+  before_filter :require_login
+    
   helper_method :current_users_list
 
-  #protected
+  #######################
+  protected
+  
   def current_users_list
     current_users.map { |u| u.username }.join(", ")
+  end
+  
+  # protected
+  def not_authenticated
+    redirect_to login_url
   end
 end
--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,4 +1,6 @@
 class PasswordResetsController < ApplicationController
+  skip_before_filter :require_login
+  
   def create
     @user = User.find_by_email(params[:email])
     @user.deliver_reset_password_instructions! if @user
